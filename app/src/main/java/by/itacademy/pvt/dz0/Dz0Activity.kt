6c05599e0a67d73657be6b208636f4af565092cf
Magomet Kochkarov <parkour09@mail.ru>
2019-06-14 16:49:00 +0300

    upload

---
+++ app/src/main/java/by/itacademy/pvt/dz0/Dz0Activity.kt
@@ -0,0 +1,44 @@
+package by.itacademy.pvt.dz0
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.itacademy.pvt.R
+import kotlinx.android.synthetic.main.activity_dz0.*
+
+class Dz0Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz0)
+
+        textA.setOnClickListener {
+            swapText()
+            swapColor()
+        }
+
+        textB.setOnClickListener {
+            swapText()
+            swapColor()
+        }
+
+        button.setOnClickListener {
+            swapText()
+            swapColor()
+        }
+
+    }
+
+    private fun swapText() {
+        val str: String = textA.text.toString()
+        textA.text = textB.text
+        textB.text = str
+    }
+
+    private fun swapColor() {
+        val temp: Drawable = textA.background
+        textA.background = textB.background
+        textB.background = temp
+    }
+
+}

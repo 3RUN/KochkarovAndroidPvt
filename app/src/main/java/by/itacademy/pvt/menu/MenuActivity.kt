0ae9491495f1cd06373a680bf687629feb1dca3d
Magomet Kochkarov <parkour09@mail.ru>
2019-06-18 16:37:39 +0300

    master update

--- app/src/main/java/by/itacademy/pvt/menu/MenuActivity.kt
+++ app/src/main/java/by/itacademy/pvt/menu/MenuActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import by.itacademy.pvt.R
-import by.itacademy.pvt.cw1.Cw1Activity
-import by.itacademy.pvt.cw2.Cw2Activity
 import by.itacademy.pvt.dz0.Dz0Activity
 import by.itacademy.pvt.dz1.Dz1Activity
 
@@ -22,30 +20,10 @@ class MenuActivity : Activity() {
                 startDz0()
             }
 
-        findViewById<View>(R.id.cwButton)
-            .setOnClickListener {
-                startCw1()
-            }
-
-        findViewById<View>(R.id.cwTwoButton)
-            .setOnClickListener {
-                startCw2()
-            }
-
         findViewById<View>(R.id.dzOneButton)
             .setOnClickListener {
                 startDz1()
             }
-    }
-
-    private fun startCw1() {
-        val intent = Intent(this, Cw1Activity::class.java)
-        startActivity(intent)
-    }
-
-    private fun startCw2() {
-        val intent = Intent(this, Cw2Activity::class.java)
-        startActivity(intent)
     }
 
     private fun startDz0() {

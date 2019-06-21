6338c45db14e95697993f5c2aa951305b9daaa94
Magomet Kochkarov <parkour09@mail.ru>
2019-06-21 15:27:03 +0300

    added boarders to CircleTransform

--- app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
@@ -1,11 +1,6 @@
 package by.itacademy.pvt.dz2
 
 import android.app.Activity
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.BitmapShader
-import android.graphics.Shader
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -14,6 +9,12 @@ import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_dz2.imageLoaderView
 import com.squareup.picasso.Transformation
+import android.graphics.Color
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Shader
+import android.graphics.BitmapShader
 
 class Dz2Activity : Activity() {
 
@@ -52,21 +53,27 @@ class Dz2Activity : Activity() {
             }
     }
 
-    class CircleTransform : Transformation {
+    inner class CircleTransform : Transformation {
+
+        private val boarderColor = Color.WHITE
+        private val boarderRadius = 15
 
         private var x: Int = 0
         private var y: Int = 0
 
         override fun transform(source: Bitmap): Bitmap {
+
             val size = Math.min(source.width, source.height)
 
             x = (source.width - size) / 2
             y = (source.height - size) / 2
 
             val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-            if (squaredBitmap !== source) source.recycle()
-            val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+            if (squaredBitmap != source) {
+                source.recycle()
+            }
 
+            val bitmap = Bitmap.createBitmap(size, size, source.config)
             val canvas = Canvas(bitmap)
             val paint = Paint()
             val shader = BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
@@ -74,7 +81,13 @@ class Dz2Activity : Activity() {
             paint.isAntiAlias = true
 
             val r = size / 2f
-            canvas.drawCircle(r, r, r, paint)
+            val paintBg = Paint()
+
+            paintBg.color = boarderColor
+            paintBg.isAntiAlias = true
+
+            canvas.drawCircle(r, r, r, paintBg)
+            canvas.drawCircle(r, r, r - boarderRadius, paint)
 
             squaredBitmap.recycle()
             return bitmap

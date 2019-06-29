02bfe1f838270c838f9f5fe3f261bfc15b419cde
Magomet Kochkarov <parkour09@mail.ru>
2019-06-29 12:12:34 +0300

    added fixes - onResume and onStop animation control

--- app/src/main/java/by/itacademy/pvt/dz5/Dz5OwlActivity.kt
+++ app/src/main/java/by/itacademy/pvt/dz5/Dz5OwlActivity.kt
@@ -8,11 +8,21 @@ import by.itacademy.pvt.R
 
 class Dz5OwlActivity : Activity() {
 
+    private val frameAnimation: AnimationDrawable =
+        findViewById<ImageView>(R.id.dz5OwlId).background as AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5owl)
+    }
 
-        val frameAnimation: AnimationDrawable = findViewById<ImageView>(R.id.dz5OwlId).background as AnimationDrawable
+    override fun onResume() {
+        super.onResume()
         frameAnimation.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        frameAnimation.stop()
     }
 }

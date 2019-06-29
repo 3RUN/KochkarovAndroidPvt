745a6bf5a9f9ab8bd754304c49350f3d7f4652b7
Magomet <parkour09@mail.ru>
2019-06-29 17:37:46 +0300

    first upload

---
+++ app/src/main/java/by/itacademy/pvt/dz6/Dz6StudentListActivity.kt
@@ -0,0 +1,13 @@
+package by.itacademy.pvt.dz6
+
+import android.app.Activity
+import android.os.Bundle
+import by.itacademy.pvt.R
+
+class Dz6StudentListActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz6list)
+    }
+}
